1123265d201da97408eec9d4a69040cdc8b6ccdf
Leila El Ghrib <leila.elghrib@gmail.com>
2019-05-28 16:17:09 +0200

    homepage

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,27 +1,32 @@
 class ActivitiesController < ApplicationController
   def index
+    if params[:location]
+      @activities = Activity.where('address LIKE ?', "%#{params[:location]}%")
+    else
+      @activities = Activity.all
+    end
   end
 
   def new
-    # @activity = Activity.new
+    @activity = Activity.new
   end
 
   def create
-    # @activity = Activity.new(activity_params)
-    # if @activity.save
-    #   redirect_to new_activity_path
-    # else
-    #   render :new
-    # end
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to new_activity_path
+    else
+      render :new
+    end
   end
 
   def show
-    # activity = Activity.find(params[:id])
+    activity = Activity.find(params[:id])
   end
 
   private
 
   def activity_params
-    # params.require(:activity).permit(:name, :address, :price, :type_activity, :description, :max_participants, :start_date, :end_date)
+    params.require(:activity).permit(:name, :address, :price, :type_activity, :description, :max_participants, :start_date, :end_date, :location)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_05_28_103412) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
