bf4d9c7b8aa354f187e996bca454556595fb5ede
Emilie Murat <thekwette@gmail.com>
2019-05-28 16:37:35 +0200

    Activities index and view

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,27 +1,28 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.all
   end
 
   def new
-    # @activity = Activity.new
+    @activity = Activity.new
   end
 
   def create
-    # @activity = Activity.new(activity_params)
-    # if @activity.save
-    #   redirect_to new_activity_path
-    # else
-    #   render :new
-    # end
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to new_activity_path
+    else
+      render :new
+    end
   end
 
   def show
-    # activity = Activity.find(params[:id])
+    activity = Activity.find(params[:id])
   end
 
   private
 
   def activity_params
-    # params.require(:activity).permit(:name, :address, :price, :type_activity, :description, :max_participants, :start_date, :end_date)
+    params.require(:activity).permit(:name, :address, :price, :type_activity, :description, :max_participants, :start_date, :end_date)
   end
 end
