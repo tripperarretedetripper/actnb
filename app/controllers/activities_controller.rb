e557b0f7212b7aae727518660c7bee6c201d6a02
Leila El Ghrib <leila.elghrib@gmail.com>
2019-05-28 13:00:15 +0200

    create controllers view and activities_controller methods <3

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,27 @@
+class ActivitiesController < ApplicationController
+  def index
+  end
+
+  def new
+    # @activity = Activity.new
+  end
+
+  def create
+    # @activity = Activity.new(activity_params)
+    # if @activity.save
+    #   redirect_to new_activity_path
+    # else
+    #   render :new
+    # end
+  end
+
+  def show
+    # activity = Activity.find(params[:id])
+  end
+
+  private
+
+  def activity_params
+    # params.require(:activity).permit(:name, :address, :price, :type_activity, :description, :max_participants, :start_date, :end_date)
+  end
+end
