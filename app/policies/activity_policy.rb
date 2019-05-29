0d6ae9bb5e0231a7e43c271c321bb859561ae613
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-05-29 11:25:17 +0200

    changed the alerts and added the pundit policies

---
+++ app/policies/activity_policy.rb
@@ -0,0 +1,22 @@
+class ActivityPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def create?
+    return true
+  end
+
+  def index?
+    return true
+  end
+
+  def new?
+    create?
+  end
+
+  def show?
+    return true
+  end
+end
