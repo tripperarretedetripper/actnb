acb707aef8f8fa4a654750420c1e3e13d1ba6871
Ana Negura <ana_n21@yahoo.com>
2019-05-29 12:02:46 +0200

    creating bookings

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -23,6 +23,7 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
+    @booking = Booking.new
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :activities, only: [ :index, :new, :show ]
-  resources :bookings, only: [ :index]
+  resources :activities, only: [ :index, :new, :show, :create ] do
+    resources :bookings, only: [ :create ]
+  end
+  resources :bookings, only: [ :index ]
 end
